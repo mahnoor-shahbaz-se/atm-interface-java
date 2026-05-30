65696098f371693908cf0f02e85c736431875540
Mahnoor Shahbaz <mahnoorshahbaz86@gmail.com>
2026-05-31 00:26:05 +0500

    ATM simulation with State Design Pattern

--- Code/ATM_State.java
+++
@@ -1,8 +0,0 @@
-package ATM_Interface;
-
-public interface ATM_State {
-    void insertCard(ATM atm);
-    void enterPin(ATM atm, int pin);
-    void selectTransaction(ATM atm);
-    void ejectCard(ATM atm);
-}
